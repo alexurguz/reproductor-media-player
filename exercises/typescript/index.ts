e7eee30fec03fad225ae15273f1d598b396f8207
Johnatan Alexis Urbano Guzman <johnurbaguz@gmail.com>
2020-09-12 16:24:11 -0500

    Video reproductor with plugins and typescript

---
+++ exercises/typescript/index.ts
@@ -0,0 +1,101 @@
+/************** BASIC TYPES **************/
+
+// Boolean
+let muted: boolean = true;
+muted = false;
+
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Diana', 'Nicole', 'Raul'];
+// people.push("9000")
+
+let peopleAndNumbers: Array<string | number | boolean> = [];
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(9001);
+peopleAndNumbers.push(true);
+
+// Enum
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul',
+  Amarillo = 'Amarillo',
+}
+
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };
+
+/************** FUNCTIONS **************/
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum = add(4, 6);
+
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+console.log(`The result to sum::::: ${fourPlus6}`);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Agente');
+console.log(richard);
+
+
+/************** INTERFACES **************/
+
+enum Colors {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+}
+
+interface Rectangulo {
+  ancho: number;
+  alto: number;
+  color?: Colors;
+}
+
+let rect: Rectangulo = {
+  ancho: 4,
+  alto: 6,
+  // color: Color.Rojo,
+};
+
+function area(r: Rectangulo): number {
+  return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function() {
+  return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+};
+
+console.log(rect.toString());
